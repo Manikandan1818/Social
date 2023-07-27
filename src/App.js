2c37aec57f97fa7c0b96f7ec0201a56b608b7fba
Manikandan1818 <simbumanii18@gmail.com>
2023-07-27 21:48:09 +0530

    Pages created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+
 function App() {
-  return <div className="App">Social App created</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
